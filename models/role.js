0ed42f0a92b636b8277d7bbbf4e6a4b6b43d5192
Abisoye Oke-lawal <abisoye.oke-lawal@andela.com>
2016-10-30 23:31:14 +0100

    Add the class method and the test for Role.all

--- models/role.js
+++ models/role.js
@@ -7,6 +7,11 @@ module.exports = function(sequelize, DataTypes) {
       associate: function(models) {
         // associations can be defined here
         models.Role.hasMany(models.User);
+      },
+      all: function(models) {
+        models.Role.findAll({}).then(function(roles){
+          return roles;
+        });
       }
     }
   });
